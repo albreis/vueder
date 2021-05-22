f801230987006f2b3844946e64163e760430c6cf
Albreis - Design & Programação <albreisdev@gmail.com>
2021-05-22 03:45:20 -0300

    1.3.0

--- README.md
+++ README.md
@@ -1,1 +1,153 @@
-# vueder
+# Vueder
+
+Componente para gerar formulários a partir de um objeto Javascript usando VueJS.
+
+## Exemplo de formulário
+``` javascript
+let Form = {
+    // Fieldset
+    dados: {
+        // Legenda do fieldset
+        legend: {
+            // Texto do fieldset
+            text: 'Fieldset A'
+            // Atributos da tag fieldset
+            attrs: {
+                class: 'classe-a',
+                title: 'Título'
+            }
+        },
+        // Atributos da tag fieldset
+        attrs: {
+        },
+        // Campos do fieldset
+        fields: {
+            // Campo
+            tipo: {
+                // Label do campo
+                label: {
+                    // Texto da label
+                    text: 'Tipo',
+                    // Atributos da tag label
+                    attrs: {
+                    }
+                },
+                // Tipo do input
+                type: 'select',
+                // Nome do input
+                name: 'tipo',
+                // Atributos da tag DIV que engloba a label e o input
+                attrs: {
+                    class: 'col-6'
+                },
+                // Atributos do container que engloba o input
+                container_attrs: {
+                    class: '',
+                    title: ''
+                },
+                // Caso seja um select, checkbox ou radio
+                // É necessario usar uma função e retornar um objeto ou array de objetos no formato: [{label: {}, value: ''}]
+                options: function(field, form, model)  {
+                    return {
+                        casa: {
+                            label: {
+                                text: 'Casa',
+                                attrs: {
+                                    class: 'col-12'
+                                }
+                            },
+                            value: 'casa'
+                        },
+                        apartamento: {
+                            label: {
+                                text: 'Apartamento',
+                                attrs: {
+                                    class: 'col-12'
+                                }
+                            },
+                            value: 'apartamento'
+                        }
+                    }
+                },
+                // Eventos para serem adicionados ao input
+                events(field, form, model) {
+                    return {
+                        change() {
+                            alert('hello world!')
+                        }
+                    }
+                }
+            },
+            estado: {
+                type: 'select',
+                label: {
+                    text: 'Estado',
+                    attrs: {
+                    }
+                },
+                name: 'estado',
+                value: '',
+                container: {
+                },
+                attrs: {
+                    class: 'row'
+                },
+                options: async function(field, form, model) {
+                    // Retorna opções de forma sincrona usando axios
+                    // Deve ser usado dessa forma para chamada de recursos externos
+                    var res = await window.axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
+                    let estados = {}
+                    for(let i in res.data) {
+                        estados[i] = {label: {
+                            text: res.data[i].nome, value: res.data[i].sigla,
+                            attrs: {
+                                class: 'col-12'
+                            }
+                    }}
+                    }
+                    this.options = (field, form, model) => estados
+                },
+                events (field, form, model) {
+                    return {
+                        // No evento 'change' do select de estado ele busca as cidades
+                        change: async function(event) {
+                            let cidades = {}
+                            if(model.estado) {
+                                var res = await window.axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${model.estado.toLowerCase()}/municipios?orderBy=nome`)
+                                for(let i in res.data) {
+                                    cidades[i] = {label: {
+                                        text: res.data[i].nome, value: res.data[i].nome,
+                                        attrs: {
+                                            class: 'col-12'
+                                        }
+                                }}
+                                }
+                            }
+                            form.endereco.fields.cidade.options = () => cidades
+                        }
+                    }
+                }
+            },
+            cidade: {
+                type: 'select',
+                name: 'cidade',
+                label: {
+                    text: 'Cidade',
+                    attrs: {
+                    }
+                },
+                value: '',
+                container: {
+                },
+                attrs: {
+                    class: 'row'
+                },
+                options: async function(field, form, model) { // eslint-disable-line
+                    return {}
+                },
+                events(field, form, model) {}
+            }
+        }
+    }
+}
+```
--- Vueder.vue
+++ Vueder.vue
@@ -1,36 +1,36 @@
 <template>
     <div id="form-builder">
         <form :method="method" :action="action" :enctype="enctype">
-            <fieldset v-for="(fieldset, key) in fieldsets" :key="key" class="row">
-                <legend v-if="fieldset.legend" class="col-12">{{fieldset.legend}}</legend>
+            <fieldset v-for="(fieldset, key) in fieldsets" :key="key" v-bind="fieldset.attrs">
+                <legend v-if="fieldset.legend" v-bind="fieldset.legend.attrs">{{fieldset.legend.text}}</legend>
                 <div class="fields">
-                    <div class="field" :data-error="validate(field)" v-for="(field, key_field) in fieldset.fields" :key="key_field" :class="field.class">
-                        <label v-if="field.label" v-html="field.label"></label>
-                        <template v-if="field.type == 'select'">
-                            <select v-on="field.events ? field.events(field, fieldsets, model) : {}" :name="field.name" v-model="field.value" @input="model[field.name] = field.value">
-                                <template v-if="field.options">
-                                    <option v-for="(option, key_option) in field.options(field, fieldsets, model)" :key="key_option" :value="option.value">
-                                        {{option.label}}
-                                    </option>
-                                </template>
-                            </select>
-                        </template>
-                        <template v-if="field.type == 'radio'">
-                            <div class="row" v-if="field.options">
-                                <label v-for="(option, key_option) in field.options(field, fieldsets, model)" :key="key_option" :value="option.value">
-                                    <input v-on="field.events ? field.events(field, fieldsets, model) : {}" type="radio" :value="option.value" v-model="field.value" @input="model[field.name] = field.value" /> <span>{{option.label}}</span>
+                    <div class="field" :data-error="validate(field)" v-for="(field, key_field) in fieldset.fields" :key="key_field" v-bind="field.attrs">
+                        <label v-bind="field.label.attrs" v-if="field.label.text" v-html="field.label.text"></label>
+                        <div v-bind="field.container">
+                            <template v-if="field.type == 'select'">
+                                <select v-bind="field.input_attrs" v-on="field.events ? field.events(field, fieldsets, model) : {}" :name="field.name" v-model="field.value" @input="model[field.name] = field.value">
+                                    <template v-if="field.options">
+                                        <option v-for="(option, key_option) in field.options(field, fieldsets, model)" :key="key_option" :value="option.value">
+                                            {{option.label.text}}
+                                        </option>
+                                    </template>
+                                </select>
+                            </template>
+                            <template v-if="field.type == 'radio' && field.options">
+                                <label v-for="(option, key_option) in field.options(field, fieldsets, model)" :key="key_option" :value="option.value" v-bind="option.attrs">
+                                    <input v-bind="field.input_attrs" v-on="field.events ? field.events(field, fieldsets, model) : {}" type="radio" :value="option.value" v-model="field.value" @input="model[field.name] = field.value" /> <span>{{option.label.text}}</span>
                                 </label>
-                            </div>
-                        </template>
-                        <template v-if="field.type == 'text' || field.type == 'password' || field.type == 'email' || field.type == 'tel' || field.type == 'number'">
-                            <input v-on="field.events ? field.events(field, fieldsets, model) : {}"  :type="field.type" v-model="field.value" @input="model[field.name] = field.value" />
-                        </template>
-                        <template v-if="field.type == 'textarea'">
-                            <textarea v-on="field.events ? field.events(field, fieldsets, model) : {}" v-model="field.value" @input="model[field.name] = field.value"></textarea>
-                        </template>
-                        <template v-if="field.type == 'component'">
-                            <component :is="field.component" />
-                        </template>
+                            </template>
+                            <template v-if="field.type == 'text' || field.type == 'password' || field.type == 'email' || field.type == 'tel' || field.type == 'number'">
+                                <input v-bind="field.input_attrs" v-on="field.events ? field.events(field, fieldsets, model) : {}"  :type="field.type" v-model="field.value" @input="model[field.name] = field.value" />
+                            </template>
+                            <template v-if="field.type == 'textarea'">
+                                <textarea v-bind="field.input_attrs" v-on="field.events ? field.events(field, fieldsets, model) : {}" v-model="field.value" @input="model[field.name] = field.value"></textarea>
+                            </template>
+                            <template v-if="field.type == 'component'">
+                                <component v-bind="field.input_attrs" :is="field.component" />
+                            </template>
+                        </div>
                     </div>
                 </div>
             </fieldset>
@@ -79,25 +79,32 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.row
+  display flex
+  flex-wrap wrap 
+  width 100%
+[class*="col-"]
+  width 100%
+for i in 1..12
+  .col-{i}
+    max-width unit(100% * (i / 12), '%')
+    padding 15px
 form
     fieldset
         margin-bottom 15px
         .fields
-            display flex
             width 100%
+            display flex
+            flex-wrap wrap
             .field
-                width 100%
-                padding 15px
                 label
                     display block
+                    margin-bottom 5px
                 &[data-error]
                     input
                     select
                     textarea
                         background #faa
-                    &:after
-                        content attr(data-error)
-                        color red
             select
             [type="text"]
             [type="password"]
