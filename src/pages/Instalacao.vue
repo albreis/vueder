b26e75a0983fb32f0ab62bd9708d7f953d520cf1
Albreis - Design & Programação <albreisdev@gmail.com>
2021-05-22 12:54:00 -0300

    nova versão

---
+++ src/pages/Instalacao.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Para utilizar o Vueder diretamente no navegador basta adicionar o seguinte:</p>
+    <iframe width="100%" height="600" src="//jsfiddle.net/ow3eqgu4/5/embedded/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Instalação',
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    devServer: {
+        host: 'everaldoreis.com.br',
+        https: false,
+        disableHostCheck: true
+    }
+}
