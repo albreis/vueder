999c5808e9697afe72e7289171ad2d14a477c37a
Albreis - Design & Programação <albreisdev@gmail.com>
2021-05-22 13:08:26 -0300

    Ajuste de URL

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/vueder/' : '/',
   devServer: {
-        public: '/vueder/',
         host: 'everaldoreis.com.br',
         https: false,
         disableHostCheck: true
